e013dc51507fd165bb3b498ac884fad8011d530a
Tom <Tom@192.168.1.37>
2014-12-23 13:20:57 +0100

    test3

---
+++ linda/src/linda/test/BasicTest3.java
@@ -0,0 +1,67 @@
+package linda.test;
+
+import linda.*;
+
+public class BasicTest3 {
+
+    public static void main(String[] a) {
+        final Linda linda = new linda.shm.CentralizedLinda();
+        // final Linda linda = new linda.server.LindaClient("//localhost:4000/MonServeur");
+                
+        for (int i = 1; i <= 3; i++) {
+            final int j = i;
+            new Thread() {  
+                public void run() {
+                    try {
+                        Thread.sleep(2);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Tuple motif = new Tuple(Integer.class, String.class);
+                    Tuple res;
+                    ((linda.shm.CentralizedLinda)linda).afficherTuples();
+                    if (j==1) {
+                    	res = linda.read(motif);
+                    } else if (j==3) {
+                    	res = linda.take(motif);
+                    } else {
+                    	res = linda.read(motif);
+                    }
+                    System.out.println("("+j+") Resultat:" + res);
+                    linda.debug("("+j+")");
+                    ((linda.shm.CentralizedLinda)linda).afficherTuples();
+                }
+            }.start();
+        }
+                
+        new Thread() {
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Tuple t1 = new Tuple(4, 5);
+                System.out.println("(0) write: " + t1);
+                linda.write(t1);
+
+                Tuple t2 = new Tuple("hello", 15);
+                System.out.println("(0) write: " + t2);
+                linda.write(t2);
+
+                linda.debug("(0)");
+
+                Tuple t3 = new Tuple(4, "foo");
+                System.out.println("(0) write: " + t3);
+                linda.write(t3);
+                linda.write(t3);
+                                
+                linda.debug("(0)");
+
+                ((linda.shm.CentralizedLinda)linda).afficherTuples();
+            }
+        }.start();
+                
+    }
+}
